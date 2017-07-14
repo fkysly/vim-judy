55d2167766b5f051449a7f6bf822cedfab7f4c9d
fkysly <fkysly@gmail.com>
2017-07-14 16:41:12 +0800

    add files && update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,20 @@ echo "installing vim-judy..."
 mv -f ~/.vim ~/.vim_old > /dev/null 2>&1
 mv -f ~/.vimrc ~/.vimrc_old > /dev/null 2>&1
 
-git clone https://github.com/fkysly/vim-judy.git ~/vim-judy
+git clone https://github.com/fkysly/vim-judy.git vim-judy
+cd vim-judy
 
-rm -rf ~/vim-judy
+cp .vimrc ~
 
-echo "done."
+mkdir ~/.vim
+cp -R ./plugin ~/.vim
+cp -R ./colors ~/.vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim -c "PluginInstall" -c "q" -c "q"
+
+cd ..
+rm -rf ./vim-judy
+
+echo "Done!"
